3c06ffe185babbb0f4a82d049c450270f84bb6a9
Ali Afarinifard <aliafarinifard@outlook.com>
2024-05-08 10:37:03 +0330

    Navbar component is done.

---
+++ src/components/Navbar.js
@@ -0,0 +1,72 @@
+import Link from "next/link"
+import Logo from "./Logo"
+import { useRouter } from "next/router";
+import { GithubIcon, LinkedInIcon } from "./Icons";
+import { motion } from "framer-motion";
+
+
+const MotionLink = motion(Link);
+
+
+const CustomLink = ({ href, title, className = "" }) => {
+
+    const router = useRouter();
+    console.log('router>>>>', router);
+
+    return (
+        <Link href={href} className={`${className} relative group`}>
+            {title}
+
+            <span
+                className={`h-[2px] inline-block bg-dark absolute left-0 -bottom-1 group-hover:w-full transition-[width] ease duration-300 ${router.asPath === href ? 'w-full' : 'w-0'}`}
+            >&nbsp;</span>
+        </Link>
+    )
+}
+
+
+const Navbar = () => {
+    return (
+        <header
+            className="w-full px-32 py-8 font-medium flex items-center justify-between"
+        >
+
+            <nav>
+                <CustomLink href={'/'} title={"Home"} className="mr-4" />
+                <CustomLink href={'/about'} title={"About"} className="mx-4" />
+                <CustomLink href={'/projects'} title={"Projects"} className="ml-4" />
+            </nav>
+
+            <nav className="flex items-center justify-center flex-wrap">
+                <MotionLink
+                    href={'https://github.com/aliafarinifard'}
+                    target="_blank"
+                    whileHover={{ y: -2 }}
+                    whileTap={{ scale: 0.9 }}
+                    className="w-6 mr-3"
+                >
+                    <GithubIcon />
+                </MotionLink>
+
+                <MotionLink
+                    href={'https://linkedin.com/in/ali-afarinifard'}
+                    target="_blank"
+                    whileHover={{ y: -2 }}
+                    whileTap={{ scale: 0.9 }}
+                    className="w-6 ml-3"
+                >
+                    <LinkedInIcon />
+                </MotionLink>
+            </nav>
+
+            <div
+                className="absolute left-[50%] top-2 translate-x-[-50%]"
+            >
+                <Logo />
+            </div>
+
+        </header>
+    )
+}
+
+export default Navbar
